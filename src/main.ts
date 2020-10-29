2ff5234d076508d142982b46bcf97dcbc5a24cbe
ibwighane <bsmwalwanda@gmail.com>
2020-10-29 11:11:42 +0200

    added global prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('nlims/api/v0/fhir');
   await app.listen(3005);
 }
 bootstrap();
