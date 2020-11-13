7d0a88936d2436034488c25ddacb1883640ba8f1
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-13 14:23:23 +0200

    feat(Fmt): add report FHIR formatter

---
+++ src/utils/diagnostic-report.formatters.ts
@@ -0,0 +1,31 @@
+import { ResourceTypesEnum } from "src/common/constants/resource-types.constants";
+import { DiagnosticReportDto } from "src/common/dtos/diagnostic-report.dto";
+import { TestResult } from "src/common/interfaces/lab-test-result";
+import { IDiagnosticReport } from "src/patients/interfaces/diagnostic-report.interface";
+
+export function toFHIRDiagnosticReport(reports: Array<TestResult>): Array<IDiagnosticReport> {
+    return reports.map((report): IDiagnosticReport => ({
+        resourceType: ResourceTypesEnum.DIAGNOSTIC_REPORT,
+        performer: [
+            {
+                reference: 'Clinton Health Access Initiative',
+                display: 'CHAI'
+            }
+        ],
+        category: [
+            {
+                coding: [
+                    {
+                        code: 'COVID',
+                        display: 'COVID-19',
+                    }
+                ],
+            },
+        ],
+        result: [
+            {
+                reference: Number(report.result) === 2 ? 'observation/positive' : 'observation/negative'
+            }
+        ]
+    }))
+}
