2b3745ebe203a35732275405544a9e4e243d54ba
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-13 14:22:53 +0200

    feat(Patients): add patients controller

--- src/patients/patients.controller.ts
+++ src/patients/patients.controller.ts
@@ -1,78 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, InternalServerErrorException, NotFoundException, Param } from '@nestjs/common';
+import { ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOkResponse } from '@nestjs/swagger';
+import { PatientDto } from 'src/common/dtos/patient.dto';
+import { LIMSPatientNotFoundException } from 'src/common/exceptions/lims-patient-not-found.exception';
+import { LIMSService } from 'src/lims/lims.service';
+import { toFHIRPatient } from 'src/utils/patient.formatters';
 import { IPatient } from './interfaces/patient.interface';
 
 @Controller('patients')
 export class PatientsController {
-  @Get('')
-  async patients(): Promise<Array<IPatient>> {
-    return [
-      {
-        resourceType: 'Patient',
-        name: [
-          {
-            use: 'official',
-            given: ['John'],
-            family: 'Doe',
-          },
-        ],
-        telecom: [
-          {
-            system: 'email',
-            value: 'john@doe.com',
-          },
-          {
-            system: 'phone',
-            use: 'mobile',
-            value: '+265888834657',
-          },
-        ],
-        address: [
-          {
-            country: 'Malawi',
-          },
-        ],
-        identifier: [
-          {
-            use: 'official',
-            value: 'MBX09499',
-          },
-        ],
-      },
-    ];
-  }
+  constructor(private readonly limsService: LIMSService) { }
+
+  @ApiOkResponse({ type: PatientDto })
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
   @Get(':id')
-  async patient(): Promise<IPatient> {
-    return {
-      resourceType: 'Patient',
-      name: [
-        {
-          use: 'official',
-          given: ['John'],
-          family: 'Doe',
-        },
-      ],
-      telecom: [
-        {
-          system: 'email',
-          value: 'john@doe.com',
-        },
-        {
-          system: 'phone',
-          use: 'mobile',
-          value: '+265888834657',
-        },
-      ],
-      address: [
-        {
-          country: 'Malawi',
-        },
-      ],
-      identifier: [
-        {
-          use: 'official',
-          value: 'MBX09499',
-        },
-      ],
-    };
+  async patient(@Param('id') id: string): Promise<IPatient> {
+    try {
+      const payload = await this.limsService.findPatientById(id);
+      if (!payload.body.length) throw new LIMSPatientNotFoundException(`Patient with id ${id} could not be found.`);
+      return toFHIRPatient(payload.body.shift());
+    } catch (error) {
+      if (error instanceof LIMSPatientNotFoundException) throw new NotFoundException(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
   }
 }
