3b47badb7f6c0b4c460ca2a757e8693f517e1e47
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-13 15:19:10 +0200

    refactor(Config): drop default values

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import { ILIMSConfig } from "src/common/interfaces/lims.config";
 
 export default function (): ILIMSConfig {
     return {
-        LIMS_API_HOST: process.env.LIMS_API_HOST || 'http://localhost:3000',
-        LIMS_API_TOKEN: process.env.LIMS_API_TOKEN || 'admin',
+        LIMS_API_HOST: process.env.LIMS_API_HOST,
+        LIMS_API_TOKEN: process.env.LIMS_API_TOKEN,
     };
 };
