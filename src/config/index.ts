051a58c82dd7d99157be3e6b84949142dc60a536
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-14 12:06:18 +0200

    refactor(Config): drop unused config file

--- src/config/index.ts
+++
@@ -1,8 +0,0 @@
-import { ILIMSConfig } from "src/common/interfaces/lims.config";
-
-export default function (): ILIMSConfig {
-    return {
-        LIMS_API_HOST: process.env.LIMS_API_HOST,
-        LIMS_API_TOKEN: process.env.LIMS_API_TOKEN,
-    };
-};
