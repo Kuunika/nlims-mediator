8b14af88b29f95e849e0df2798b1a487f1ab8953
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-14 12:06:04 +0200

    refactor(Config): revert to config module defaults

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PatientsModule } from './patients/patients.module';
-import LIMSConfig from './config';
 import { LIMSModule } from './lims/lims.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ load: [LIMSConfig], isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true }),
     PatientsModule,
     LIMSModule
   ]
