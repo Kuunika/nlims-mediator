d8cf8f5104f370a75734577255252531678cf294
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-11-16 13:07:58 +0200

    chore(Patients): register test providers

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { LIMSModule } from 'src/lims/lims.module';
 import { PatientsDiagnosticReportsController } from './patients-diagnostic-reports.controller';
 import { PatientsController } from './patients.controller';
+import { TestPatientService } from './test-patient.service';
+import { TestPatientsController } from './test-patients.controller';
 
 @Module({
   imports: [LIMSModule],
-  controllers: [PatientsController, PatientsDiagnosticReportsController]
+  controllers: [
+    PatientsController,
+    PatientsDiagnosticReportsController,
+    TestPatientsController
+  ],
+  providers: [TestPatientService]
 })
 export class PatientsModule {}
