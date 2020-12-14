e6fd147c4e7dee4ed9ab45fa606c8843ce0ab9f1
ibwighane <bsmwalwanda@gmail.com>
2020-12-14 21:11:29 +0200

    added date tested

--- src/utils/diagnostic-report.formatters.ts
+++ src/utils/diagnostic-report.formatters.ts
@@ -1,30 +1,35 @@
-import { ResourceTypesEnum } from "src/common/constants/resource-types.constants";
-import { TestResult } from "src/common/interfaces/lab-test-result";
-import { IDiagnosticReport } from "src/patients/interfaces/diagnostic-report.interface";
+import { ResourceTypesEnum } from 'src/common/constants/resource-types.constants';
+import { TestResult } from 'src/common/interfaces/lab-test-result';
+import { IDiagnosticReport } from 'src/patients/interfaces/diagnostic-report.interface';
 
-export function toFHIRDiagnosticReport(reports: Array<TestResult>): Array<IDiagnosticReport> {
-    return reports.map((report): IDiagnosticReport => ({
-        resourceType: ResourceTypesEnum.DIAGNOSTIC_REPORT,
-        performer: [
+export function toFHIRDiagnosticReport(
+  reports: Array<TestResult>,
+): Array<IDiagnosticReport> {
+  return reports.map(
+    (report): IDiagnosticReport => ({
+      resourceType: ResourceTypesEnum.DIAGNOSTIC_REPORT,
+      performer: [
+        {
+          reference: 'Clinton Health Access Initiative',
+          display: 'CHAI',
+        },
+      ],
+      effectiveDate: report.datetested,
+      category: [
+        {
+          coding: [
             {
-                reference: 'Clinton Health Access Initiative',
-                display: 'CHAI'
-            }
-        ],
-        category: [
-            {
-                coding: [
-                    {
-                        code: 'COVID',
-                        display: 'COVID-19',
-                    }
-                ],
+              code: 'COVID',
+              display: 'COVID-19',
             },
-        ],
-        result: [
-            {
-                reference: `observation/${report.result.toLowerCase()}`
-            }
-        ]
-    }))
+          ],
+        },
+      ],
+      result: [
+        {
+          reference: `observation/${report.result.toLowerCase()}`,
+        },
+      ],
+    }),
+  );
 }
